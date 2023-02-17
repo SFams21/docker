c3a08593c04d7e220d6a6d8432430d053201cb0b
Riyan Sipayung <riyandbreaks331@gmail.com>
2023-02-18 05:07:07 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,10 @@ figlet -t -f smmono9 "RIYAN SIPAYUNG"
 printf "${STOP}"
 neofetch --stdout
 printf "\n\n"
-printf "\n\n"
 node -v
-printf "\n\n"
 printf "\n\n"
 printf "${RED} VIP DOCKER EGG RIYAN SIPAYUNG !!${STOP}"
 echo ""
 cd /home/container
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=`eval echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 ${MODIFIED_STARTUP}
